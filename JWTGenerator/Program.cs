65903f3557ef52fcb296b7f962da7cc33db1ad7a
Alex Pritea <pirtea.a@gmail.com>
2021-12-08 12:41:41 +0200

    Add project files.

---
+++ JWTGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+
+namespace JWTGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var payload = new Dictionary<string, object>
+            {
+                { "points", 555 },
+                { "userKey", "" }
+            };
+            const string secret = "";
+
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            var token = encoder.Encode(payload, secret);
+            Console.WriteLine(token);
+        }
+    }
+}
